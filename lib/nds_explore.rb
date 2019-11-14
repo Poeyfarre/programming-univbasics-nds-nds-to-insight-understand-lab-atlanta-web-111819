7faecba8bdc642b7c3fb3d89709072badc86f6df
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-14 16:57:26 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,4 +8,11 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  new_movies = directors_database[0][:movies]
+  movies_index = 0
+  while movies_index < new_movies.count do
+    puts new_movies[movies_index][:title]
+    movies_index += 1
 end
+end
+
